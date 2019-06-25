64df687c3a0d267085b7d78552696d109474c766
NatalyaDol <suhoikova@gmail.com>
2019-06-25 12:33:54 +0300

    Add the comments and comment components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-ro
 
 import Layout from './app/layouts/Layout';
 import MainPage from './app/pages/Main';
-import Users from './app/pages/Users';
 import PageNotFound from './app/pages/PageNotFound';
+import Users from './app/pages/Users';
 import User from './app/pages/User';
 import Posts from './app/pages/Posts';
 import Post from './app/pages/Post';
+
+import Comments from './app/pages/Comments';
+import Comment from './app/pages/Comment';
+
 
 //это машрутизатор
 ReactDOM.render(<Router history={browserHistory}>
@@ -22,6 +26,10 @@ ReactDOM.render(<Router history={browserHistory}>
       <Route path="posts" component={Posts}>
         <Route path=":postId" component={Post}/>
       </Route>
+
+      <Route path="comments" component={Comments}>
+        <Route path=":commentId" component={Comment}/>
+      </Route>      
     <Route path="*" component={PageNotFound}/>
   </Route>
 </Router>, document.querySelector('#root'));
